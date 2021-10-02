cb53fcbd97c96abb4c6c4a1d4db856aa225cb03d
Poottipong Rueangsuntikornkul <6210520117@cn330>
2021-10-02 18:42:36 +0700

    Index test

--- cn331project1/courses/tests.py
+++ cn331project1/courses/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from .models import CourseModel
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.contrib.auth.hashers import make_password
 # Create your tests here.
+
+class CourseTestCase(TestCase):
+    
+    def setUp(self):
+        course1 = CourseModel.objects.create(code='CN000', name='test', semester=1, year=1, seat=1, status=True)
+        student = User.objects.create(username='user1', password='1234')
+    
+    def test_courses_index(self):
+        c = Client()
+        response = c.get(reverse('courses:index'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_course(self):
+        c = Client()
+        course = CourseModel.objects.first()
+        response = c.get(reverse('courses:course', args=(course.code,)))
+        self.assertEqual(response.status_code, 200)
+
+        
--- cn331project1/home/tests.py
+++ cn331project1/home/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.urls import reverse
 # Create your tests here.
+class HomeTestCase(TestCase):
+
+    def test_courses_index(self):
+        c = Client()
+        response = c.get(reverse('home:index'))
+        self.assertEqual(response.status_code, 200)
